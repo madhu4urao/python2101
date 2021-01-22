a5c7a2b01e582a2480316e197e8fd6197cfedac0
madhu4urao <madhusw511@gmail.com>
2021-01-22 11:44:52 +0530

    hello

---
+++ poly.py
@@ -0,0 +1,26 @@
+class Cat:
+    def __init__(self,name,age):
+        self.name =name
+        self.age =age
+    def info(self):
+        print(f"hi this is {self.name}. I am {self.age} years old.")
+    def make_sound(self):
+        print("Meow")
+class Dog:
+    def __init__(self,name,age):
+        self.name =name
+        self.age =age
+
+    def info(self):
+        print(f"hi this is {self.name}. I am {self.age} years old.")
+
+    def make_sound(self):
+        print("woof")
+
+cat1 =Cat("cuty", 2.5)
+dog1 =Dog("tommy", 4)
+# cat1.info()
+# dog1.info()
+for animal in (cat1,dog1):
+    animal.info()
+    animal.make_sound()
